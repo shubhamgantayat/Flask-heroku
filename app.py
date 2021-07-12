ff0d3dd45437101f6b37ca177564ef717c3f964b
Shubham Gantayat <sgantayat9@gmail.com>
2021-07-12 16:06:06 +0530

    fifth Commit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import render_template, jsonify, Flask, request
 from prime import prime
 
@@ -29,4 +31,5 @@ def check_prime():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)

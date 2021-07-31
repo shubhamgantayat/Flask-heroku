8e304b1fa189bdd5e98c8f41700c2cfe2edfc362
Shubham Gantayat <sgantayat9@gmail.com>
2021-07-31 21:03:10 +0530

    app fixed

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-import os
-
 from flask import render_template, jsonify, Flask, request, send_from_directory
-from flask_restful import Api,Resource,reqparse
 from prime import prime
+from flask_cors import cross_origin
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-api = Api(app)
-
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
 
 @app.route('/', defaults={'path':''})
+@cross_origin()
 def home_page(path):
     return send_from_directory(app.static_folder,'index.html')
 
 
 @app.route('/prime', methods=['POST'])
+@cross_origin()
 def check_prime():
     if request.method == 'POST':
         string = ''
@@ -36,8 +30,6 @@ def check_prime():
         finally:
             return render_template('form.html',results = string)
 
-
-api.add_resource(HelloWorld,'/flask/hello')
 
 if __name__ == '__main__':
     app.run(debug=True)
